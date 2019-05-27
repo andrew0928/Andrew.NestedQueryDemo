11886120187cfc8b5da8fa982d5434b8e191e05f
Andrew Wu <chicken@chicken-house.net>
2019-05-28 02:33:39 +0800

    draft with solution #1 (article) complete

--- DIRDB/demo1-tree-operation.sql
+++ DIRDB/demo1-tree-operation.sql
@@ -12,17 +12,95 @@
 --
 
 declare @root int;
-select @root = ID from demo1.DIRINFO where PARENT_ID = 1 and name = 'windows'
+select @root = D2.ID
+from demo1.DIRINFO D1 inner join demo1.DIRINFO D2 on D1.ID = D2.PARENT_ID 
+where D1.NAME = 'c:\' and D2.NAME = 'windows'
 
+-- list DIR
+select NAME from demo1.DIRINFO where PARENT_ID = @root
+
+-- list FILE
+select NAME, SIZE from demo1.FILEINFO where DIR_ID = @root
+
+
+-- formatting output
 select * from
 (
-	select '<DIR>' as type, NULL as size, name from demo1.DIRINFO where PARENT_ID = @root
+	select '<DIR>' as type, name, NULL as size from demo1.DIRINFO where PARENT_ID = @root
 	union
-	select '' as type, Size as size, NAME as name from demo1.FILEINFO where DIR_ID = @root
-) F
+	select '' as type, NAME as name, SIZE as size from demo1.FILEINFO where DIR_ID = @root
+) IX
 order by name asc
 
 
 --
 -- 1. 模擬 dir /s /b c:\windows\*.dll，找出所有位於 c:\windows 目錄下 (包含子目錄) 所有副檔名為 .dll 的檔案清單
 --
+
+
+declare @root int;
+
+select @root = D3.ID
+from
+	demo1.DIRINFO D1 inner join 
+	demo1.DIRINFO D2 on D1.ID = D2.PARENT_ID inner join
+	demo1.DIRINFO D3 on D2.ID = D3.PARENT_ID
+
+where D1.NAME = 'c:\' and D2.NAME = 'windows' and D3.NAME = 'system32'
+
+
+;with DIR_CTE(ID, NAME, FULLNAME) as
+(
+	select ID, NAME, cast('./' + NAME as ntext)
+	from demo1.DIRINFO
+	where PARENT_ID = @root
+
+	union all
+
+	select D1.ID, D1.NAME, cast(concat(D2.FULLNAME, '/', D1.NAME) as ntext)
+	from demo1.DIRINFO D1 inner join DIR_CTE D2 on D1.PARENT_ID = D2.ID
+)
+
+
+select concat(D.FULLNAME, '/', F.NAME) as NAME, F.SIZE
+from DIR_CTE D inner join demo1.FILEINFO F on D.ID = F.DIR_ID
+where F.EXT = '.ini'
+
+
+
+
+
+
+
+
+--
+-- mkdir
+--
+
+
+declare @root int;
+
+-- step 1, 找出 c:\windows 的 ID
+select @root = D2.ID
+from
+	demo1.DIRINFO D1 inner join 
+	demo1.DIRINFO D2 on D1.ID = D2.PARENT_ID 
+where D1.NAME = 'c:\' and D2.NAME = 'windows' 
+
+
+-- step 2, 在 c:\windows 下新增子目錄 backup
+begin tran
+insert demo1.DIRINFO (PARENT_ID, NAME) values (@root, 'backup');
+select @@identity
+
+select * from demo1.DIRINFO where PARENT_ID = @root order by NAME asc
+rollback
+
+
+
+
+--
+-- move
+--
+
+
---
+++ DIRDB/demo1.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [demo1]
+    AUTHORIZATION [dbo];
+
